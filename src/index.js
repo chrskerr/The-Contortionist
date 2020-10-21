eabcf454081cfd32e7fa38cc405db7f489a79c02
Chris Kerr <chriskerr@me.com>
2020-10-21 16:05:33 +1100

    Re-added serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 
 // dev
 import React from "react";
-import ReactDOM from "react-dom";
+import { hydrate, render } from "react-dom";
 
 // app
 import "./index.scss";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 //
 // Index 
 //
 
-ReactDOM.render(
-	<App />,
-	document.getElementById( "root" ),
-);
+const rootElement = document.getElementById( "root" );
+if ( rootElement.hasChildNodes()) {
+	hydrate( <App />, rootElement );
+} else {
+	render( <App />, rootElement );
+}
+
+serviceWorker.register();
